8df5db0541146a040a9fbd5e417e71bd376ed0f0
Jonathan_Chan <Jonathanchan.808@gmail.com>
2024-03-05 17:01:05 -0500

    Added notes to 217

--- arrays_&_hashing/containsDuplicate.py
+++ arrays_&_hashing/containsDuplicate.py
@@ -3,4 +3,9 @@
 '''        
 
 def containsDuplicate(self, nums):
+    # Return true if any value appears more thatn 2x in the array
+    # otherwise return false if every element is distinctive
+
+    # Would be great try to create a hashmap to show what numbers have appeared
+    
     pass
