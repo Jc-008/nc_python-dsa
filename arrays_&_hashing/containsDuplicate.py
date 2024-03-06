15d92ca963a207040fdc08099c385efef82f0ad3
Jonathan_Chan <Jonathanchan.808@gmail.com>
2024-03-06 00:00:58 -0500

    Added notes and my solution that passed

--- arrays_&_hashing/containsDuplicate.py
+++ arrays_&_hashing/containsDuplicate.py
@@ -2,10 +2,30 @@
     def containsDuplicate(self, nums: List[int]) -> bool:
 '''        
 
-def containsDuplicate(self, nums):
+def containsDuplicate(nums):
     # Return true if any value appears more thatn 2x in the array
     # otherwise return false if every element is distinctive
 
     # Would be great try to create a hashmap to show what numbers have appeared
-    
-    pass
+
+    # Create a hashmap to take down the frequency of the values that are in the array
+    # loop through the array and add to in the values
+    # if the value is more than 2 we can short circuit and return true
+
+
+    hashmap = {}
+
+    for num in nums:
+        if num in hashmap:          # If the element is already in the hashmap, increment its frequency
+            hashmap[num] += 1
+        else:                       # If the element is not in the hashmap, add it with a frequency of 1
+            hashmap[num] = 1 
+
+
+    # It iterates over each value in the hashmap.values() and checks if the value is greater than or equal to 2. It produces a sequence of True or False values for each element.
+    return any(value >= 2 for value in hashmap.values()) 
+
+
+# Example usage:
+nums = [1,2,3,4]
+result = containsDuplicate(nums)
