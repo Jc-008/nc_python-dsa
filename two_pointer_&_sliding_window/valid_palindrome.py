b92fceee83ccd914424f4169d178d4f44e03c7c5
Jonathan_Chan <Jonathanchan.808@gmail.com>
2024-03-12 16:47:35 -0400

    Added my solution and comments

--- two_pointer_&_sliding_window/valid_palindrome.py
+++ two_pointer_&_sliding_window/valid_palindrome.py
@@ -4,15 +4,38 @@ def isPalindrome(s) -> bool:
     
     # convert all uppercase letters to lowercase
     # remove all spaces
-    # Using 2 pointers, traverse the string makeing sure each pointer is pointing to the same letter, if not return false
+    # Using 2 pointers, traverse the string making sure each pointer is pointing to the same letter, if not return false
+   
+   
+   lower_cased = s.lower()
+   print(lower_cased)
 
+   noSpacingWords = lower_cased.replace(" ", "").replace(",", "").replace(":", "")
+   print(len(noSpacingWords), "length")
+   left, right = 0, len(noSpacingWords ) - 1
+#    or 
+#    noSpacingWords = ''.join(char for char in lower_cased if char.isalnum())
+   print(noSpacingWords, 'nospacing')
+
+   while left < right:
+      leftLetter = noSpacingWords[left]
+      rightLetter = noSpacingWords[right]
+      print(left, leftLetter)
+      print(right, rightLetter)
+
+
+      if leftLetter == rightLetter:
+         left += 1
+         right -= 1
+      else: 
+        return False 
+   
+   return True
     
 
 
 # Example usage:
-    s = "A man, a plan, a canal: Panama"
+s = "A man, a plan, a canal: Panama"
 # s = "race a car"
 # s = " "
-# nums = [1,2,3,4]
-# nums = [1,1,1,3,3,4,3,2,4,2]
 result = isPalindrome(s)
