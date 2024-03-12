1871de1d2b2367cc6157ebcabb959de64a04b6ee
Jonathan_Chan <Jonathanchan.808@gmail.com>
2024-03-12 15:51:06 -0400

    Added example and notes

--- two_pointer_&_sliding_window/valid_palindrome.py
+++ two_pointer_&_sliding_window/valid_palindrome.py
@@ -1,2 +1,18 @@
 def isPalindrome(s) -> bool:
-    pass
+    # Return true if it is a plaindrome, otherwise false
+    # This can use a two pointer strategy
+    
+    # convert all uppercase letters to lowercase
+    # remove all spaces
+    # Using 2 pointers, traverse the string makeing sure each pointer is pointing to the same letter, if not return false
+
+    
+
+
+# Example usage:
+    s = "A man, a plan, a canal: Panama"
+# s = "race a car"
+# s = " "
+# nums = [1,2,3,4]
+# nums = [1,1,1,3,3,4,3,2,4,2]
+result = isPalindrome(s)
